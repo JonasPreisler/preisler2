67d95d02e2a4f5faecfbf72923200a3117e7609c
Jonas Preisler <jonas.preisler@gmail.com>
2017-01-19 04:33:46 +0100

    commit 7

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170118153854) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "categories", force: :cascade do |t|
     t.string "name"
   end
